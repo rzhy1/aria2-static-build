8090d78ef4604772e21521bdf1ec572e5657ce08
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-05-24 23:02:26 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -220,6 +220,7 @@ prepare_zlib() {
       -DWITH_GTEST=OFF
     cmake --build build
     cmake --install build
+    cat "${CROSS_PREFIX}/lib/pkgconfig/zlib.pc"
     zlib_ver="$(grep Version: "zlib.pc")"
     echo "- zlib-ng: ${zlib_ng_ver}, source: ${zlib_ng_latest_url:-cached zlib-ng}" >>"${BUILD_INFO}"
     # Fix mingw build sharedlibdir lost issue
