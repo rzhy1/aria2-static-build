3ce23f497af7d6b588f928cdee54392c6d67bc9c
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-05-24 23:14:47 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -220,6 +220,15 @@ prepare_zlib() {
       -DWITH_GTEST=OFF
     cmake --build build
     cmake --install build
+    # 搜索指定文件
+    file_path=$(find / -name "zlib.pc" 2>/dev/null)
+
+    # 检查文件是否存在
+    if [ -n "$file_path" ]; then
+        echo "文件位置：$file_path"
+    else
+        echo "文件未找到"
+    fi
     cat "${CROSS_PREFIX}/lib/pkgconfig/zlib-ng.pc"
     zlib_ver="$(grep Version: "zlib.pc")"
     echo "- zlib-ng: ${zlib_ng_ver}, source: ${zlib_ng_latest_url:-cached zlib-ng}" >>"${BUILD_INFO}"
