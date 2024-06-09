3964f7c8d9a4c00d37acd3d8b98bbd8c3d232db9
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-06-09 14:28:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,7 +15,10 @@ set -euo pipefail
 HOST=x86_64-w64-mingw32
 PREFIX=/usr/local/$HOST
 
-export DEBIAN_FRONTEND=noninteractive
+# 配置 apt 以保留下载的 .deb 包，并禁用 HTTPS 证书验证
+rm -f /etc/apt/apt.conf.d/*
+echo 'Binary::apt::APT::Keep-Downloaded-Packages "true";' >/etc/apt/apt.conf.d/01keep-debs
+echo -e 'Acquire::https::Verify-Peer "false";\nAcquire::https::Verify-Host "false";' >/etc/apt/apt.conf.d/99-trust-https
 
 apt-get update
 DEBIAN_FRONTEND="noninteractive" apt-get upgrade -y
