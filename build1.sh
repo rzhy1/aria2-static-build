73a1f1ee6f6375790b32de8744e7df28b2b0b0c4
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-10-13 21:59:54 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -69,18 +69,18 @@ apt update
 echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - 升级已安装的软件包⭐⭐⭐⭐⭐⭐"
 apt upgrade -y
 echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - 安装软件包⭐⭐⭐⭐⭐⭐"
-#apt install -y --no-install-recommends \
-#  autoconf \
-#  automake \
-#  autopoint \
-#  x86_64-w64-mingw32-g++ \
-#  jq \
-#  libtool \
-#  make \
-#  pkgconf \
-#  wget \
-#  tcl \
-#  unzip
+apt install -y --no-install-recommends \
+  autoconf \
+  automake \
+  autopoint \
+  x86_64-w64-mingw32-g++ \
+  jq \
+  libtool \
+  make \
+  pkgconf \
+  wget \
+  tcl \
+  unzip
 
 BUILD_ARCH="$(gcc -dumpmachine)"
 TARGET_ARCH="${CROSS_HOST%%-*}"
