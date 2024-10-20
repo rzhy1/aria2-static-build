6fa4dc73af06a787f7bd7738e6966345badab158
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-10-20 12:35:10 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -416,11 +416,13 @@ prepare_ninja
 echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - 下载并编译 zlib、xz、libxml2、sqlite、c_ares、libssh2⭐⭐⭐⭐⭐⭐"
 prepare_zlib &
 prepare_xz &
-prepare_libxml2 &
+
 prepare_sqlite &
 prepare_c_ares &
 prepare_libssh2 &
 wait
+echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - 下载并编译 libxml2⭐⭐⭐⭐⭐⭐"
+prepare_libxml2 
 echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - 下载并编译 aria2⭐⭐⭐⭐⭐⭐"
 build_aria2
 echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - 编译完成⭐⭐⭐⭐⭐⭐"
