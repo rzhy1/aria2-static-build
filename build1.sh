3d2e670aded4675f0fb857bf2ddb5241b07ceb03
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-11-03 01:36:47 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -7,7 +7,9 @@ export CROSS_HOST="x86_64-w64-mingw32"
 export CROSS_ROOT="/cross_root"
 export PATH="${CROSS_ROOT}/bin:${PATH}"
 export CROSS_PREFIX="${CROSS_ROOT}/${CROSS_HOST}"
+export LD=lld
 lld --version
+x86_64-w64-mingw32-lld --version
 set -o pipefail
 export USE_ZLIB_NG="${USE_ZLIB_NG:-1}"
 retry() {
