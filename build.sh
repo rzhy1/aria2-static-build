3defbf5cfd2c0a15faa72657c53c95425b6f8686
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-11-03 21:46:39 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -142,8 +142,8 @@ STRIP=$HOST-strip \
     --libdir=$PREFIX/lib \
     --includedir=$PREFIX/include \
     --static \
-     CFLAGS="-O3 -g0" \
-     CXXFLAGS="-O3 -g0"
+     CFLAGS="-O2 -g0" \
+     CXXFLAGS="-O2 -g0"
 make -j$(nproc) install
 echo "| zlib | ${zlib_tag} | ${zlib_latest_url} |" >>"${BUILD_INFO}"
 cd ..
