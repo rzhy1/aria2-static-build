bdb5ca3a26cd8bd64f50308ac89a0fc716d9f9e8
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-11-04 17:14:46 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -280,7 +280,7 @@ prepare_c_ares() {
   if [ ! -f "./configure" ]; then
     autoreconf -i
   fi
-  CFLAGS="-O2 -g0 -flto=$(nproc)" CXXFLAGS="-O2 -g0 -flto=$(nproc)" ./configure --host="${CROSS_HOST}" --prefix="${CROSS_PREFIX}" --enable-static --disable-shared --enable-silent-rules --disable-tests --without-random
+  CFLAGS="-O3 -g0 -flto=$(nproc)" CXXFLAGS="-O3 -g0 -flto=$(nproc)" ./configure --host="${CROSS_HOST}" --prefix="${CROSS_PREFIX}" --enable-static --disable-shared --enable-silent-rules --disable-tests --without-random
   make -j$(nproc)
   make install
   cares_ver="$(grep 'Version:' "${CROSS_PREFIX}/lib/pkgconfig/libcares.pc" | awk '{print $2}')"
