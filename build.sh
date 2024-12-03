19fa821d8d17893141298ebb67b3ad0a1551dc82
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-12-04 00:04:23 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ if [ "$USE_GCC15" -eq 1 ]; then
     curl -SLf -o "/tmp/mingw-w64-x86_64-toolchain.zip" "https://github.com/rzhy1/build-mingw-w64/releases/download/mingw-w64/mingw-w64-x86_64-toolchain.zip"
     unzip -o "/tmp/mingw-w64-x86_64-toolchain.zip" -d "/usr/"
     apt-get update 
-    apt-get install -y --no-install-recommends libgmp
+    apt-get install -y --no-install-recommends libgmp-dev
 else
     echo "使用相对成熟的 mingw-w64-x86_64-toolchain (GCC 14)..."
     curl -SLf -o "/tmp/x86_64-w64-mingw32.tar.xz"  "https://github.com/rzhy1/musl-cross/releases/download/mingw-w64/x86_64-w64-mingw32.tar.xz"
