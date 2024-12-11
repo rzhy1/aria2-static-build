e9419ac574672061f99132b985f7b528354b652d
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-12-11 13:40:04 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ if [ "$USE_GCC15" -eq 1 ]; then
     curl -SLf -o "/tmp/mingw-w64-x86_64-toolchain.gz" "https://github.com/rzhy1/build-mingw-w64/releases/download/mingw-w64/mingw-w64-x86_64-toolchain.gz"
     gzip -d /tmp/mingw-w64-x86_64-toolchain.gz
     tar -xvf "/tmp/mingw-w64-x86_64-toolchain"  --strip-components=1 -C "/usr/"
-    find / name "libipxsap.a"
+    find / -name "libipxsap.a"
 else
     echo "使用相对成熟的 mingw-w64-x86_64-toolchain (GCC 14)..."
     curl -SLf -o "/tmp/x86_64-w64-mingw32.tar.xz"  "https://github.com/rzhy1/musl-cross/releases/download/mingw-w64/x86_64-w64-mingw32.tar.xz"
