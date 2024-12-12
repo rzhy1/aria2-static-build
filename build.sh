4653fbf35bc53abc659e8252c80040aa3ce93643
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-12-12 22:13:36 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ USE_GCC15=1
 if [ "$USE_GCC15" -eq 1 ]; then
     echo "使用最新版的 mingw-w64-x86_64-toolchain (GCC 15)..."
     curl -SLf -o "/tmp/mingw-w64-x86_64-toolchain.tar.zst" "https://github.com/rzhy1/build-mingw-w64/releases/download/mingw-w64/mingw-w64-x86_64-toolchain.tar.zst"
-    tar --zstd -xf "/tmp/x86_64-w64-mingw32.tar.zst" --strip-components=1 -C "/usr/"
+    tar --zstd -xf "/tmp/mingw-w64-x86_64-toolchain.tar.zst" --strip-components=1 -C "/usr/"
 else
     echo "使用相对成熟的 mingw-w64-x86_64-toolchain (GCC 14)..."
     curl -SLf -o "/tmp/x86_64-w64-mingw32.tar.xz"  "https://github.com/rzhy1/musl-cross/releases/download/mingw-w64/x86_64-w64-mingw32.tar.xz"
