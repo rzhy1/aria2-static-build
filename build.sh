bc9fb5f0035d2ab08134d760d7c60ec6f9b5010b
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-12-30 21:58:36 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -36,7 +36,8 @@ duration1=$(echo "$end_time - $start_time" | bc | xargs printf "%.1f")
 
 echo "x86_64-w64-mingw32-gcc版本是："
 x86_64-w64-mingw32-gcc --version
-x86_64-w64-mingw32-gcc -print-search-dirs
+#x86_64-w64-mingw32-gcc -print-search-dirs
+
 # 配置 apt 以保留下载的 .deb 包，并禁用 HTTPS 证书验证
 #rm -f /etc/apt/apt.conf.d/*
 #echo 'Binary::apt::APT::Keep-Downloaded-Packages "true";' >/etc/apt/apt.conf.d/01keep-debs
@@ -270,7 +271,7 @@ autoreconf -i
     --disable-checking \
     ARIA2_STATIC=yes \
     CPPFLAGS="-I$PREFIX/include" \
-    LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib -static -lgmp" \
+    LDFLAGS="-static -L$PREFIX/lib  $PREFIX/lib/libgmp.a -Wl,-rpath,$PREFIX/lib" \
     PKG_CONFIG="/usr/bin/pkg-config" \
     PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig" \
     CFLAGS="-O2 -g0 -flto=$(nproc)" \
