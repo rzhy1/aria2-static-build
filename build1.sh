e13feaf9a2ac46e016ef9e2cfb4d96531a59b180
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-01-01 16:11:36 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -174,10 +174,8 @@ prepare_zlib() {
       -DWITH_GTEST=OFF
     cmake --build build
     cmake --install build
-    sed -i "s/^Version: .*/Version: ${zlib_ng_latest_tag}/" "${CROSS_PREFIX}/lib/pkgconfig/zlib.pc"
     zlib_ng_ver="${zlib_ng_latest_tag}($(grep '^Version:' ${CROSS_PREFIX}/lib/pkgconfig/zlib.pc | awk '{print $2}'))"
     echo "| zlib-ng | ${zlib_ng_ver} | ${zlib_ng_latest_url:-cached zlib-ng} |" >>"${BUILD_INFO}" || exit
-    cat "${CROSS_PREFIX}/lib/pkgconfig/zlib.pc" | grep "^Version:"
     # Fix mingw build sharedlibdir lost issue
     sed -i 's@^sharedlibdir=.*@sharedlibdir=${libdir}@' "${CROSS_PREFIX}/lib/pkgconfig/zlib.pc"
   else
