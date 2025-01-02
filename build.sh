43d7b4d57dd236502c99ac24be02c88d3e4ade1c
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-01-02 08:04:27 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ BUILD_INFO="${SELF_DIR}/build_info.md"
 echo "$BUILD_INFO"
 echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - 下载最新版mingw-w64⭐⭐⭐⭐⭐⭐"
 start_time=$(date +%s.%N)
-USE_GCC15=1
+USE_GCC15=0
 if [ "$USE_GCC15" -eq 1 ]; then
     echo "使用最新版的 mingw-w64-x86_64-toolchain (GCC 15)..."
     curl -SLf -o "/tmp/mingw-w64-x86_64-toolchain.tar.zst" "https://github.com/rzhy1/build-mingw-w64/releases/download/mingw-w64/mingw-w64-x86_64-toolchain.tar.zst"
