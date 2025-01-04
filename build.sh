373492b51e35061e34958b3d42209a5abcc67d8b
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-01-04 21:27:10 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -92,6 +92,9 @@ if [ "$USE_GCC15" -eq 1 ]; then
     x86_64-w64-mingw32-nm -B /usr/x86_64-w64-mingw32/lib/libgmp.a | grep __gmpz_init
     ls $PREFIX/lib | grep libgmp
     ls $PREFIX/include | grep gmp.h
+    echo "查找pkg-config --libs gmp"
+    pkg-config --libs gmp
+    pkg-config --cflags gmp
     find / -name "libgmp.pc" 2>/dev/null
     cat $PREFIX/lib/pkgconfig/libgmp.pc
 fi
