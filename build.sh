df70b3b77e68fafb4bcde428ea9086d8e8eace23
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-01-04 22:56:11 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -77,23 +77,14 @@ cd gmp-*
 ./configure \
     --disable-shared \
     --enable-static \
-    --enable-pkg-config \
     --prefix=$PREFIX \
     --host=$HOST \
-    --build=$(dpkg-architecture -qDEB_BUILD_GNU_TYPE) \
     --disable-cxx \
     --enable-fat \
     CFLAGS="-mtune=generic -O2 -g0" \
     CXXFLAGS="-mtune=generic -O2 -g0"
 make -j$(nproc) install
 echo "| gmp | ${gmp_tag} | https://ftp.gnu.org/gnu/gmp/gmp-${gmp_tag}.tar.xz |" >>"${BUILD_INFO}"
-if [ "$USE_GCC15" -eq 1 ]; then 
-    #cp "$PREFIX/lib/libgmp.a" "/usr/x86_64-w64-mingw32/lib/"
-    echo “查看 libgmp.a 文件中是否有 __gmpz_init 符号”
-    x86_64-w64-mingw32-nm -B /usr/x86_64-w64-mingw32/lib/libgmp.a | grep __gmpz_init
-fi
-echo "查找libgmp.pc"
-find / -name "libgmp.pc" 2>/dev/null
 cd ..
 end_time=$(date +%s.%N)
 duration2=$(echo "$end_time - $start_time" | bc | xargs printf "%.1f")
