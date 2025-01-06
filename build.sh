2fa80b42097d3c995df18dc2a0db6d4ae6593167
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-01-06 23:49:10 +0800

    LDFLAGS="-L$PREFIX/lib -Wl,--threads=$(nproc)"

--- build.sh
+++ build.sh
@@ -268,7 +268,7 @@ autoreconf -i
     --with-sysroot=$PREFIX \
     ARIA2_STATIC=yes \
     CPPFLAGS="-I$PREFIX/include" \
-    LDFLAGS="-L$PREFIX/lib -Wl,--thinlto-jobs=$(nproc)" \
+    LDFLAGS="-L$PREFIX/lib -Wl,--threads=$(nproc)" \
     PKG_CONFIG="/usr/bin/pkg-config" \
     PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
 make -j$(nproc)
