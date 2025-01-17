4ae637da2ff29e814fb8640b4bd79737554d3c19
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-01-17 22:34:14 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -261,6 +261,7 @@ prepare_sqlite() {
   echo "| sqlite | ${sqlite_ver} | ${sqlite_latest_url:-cached sqlite} |" >>"${BUILD_INFO}"
   pkg-config --libs sqlite3
   pkg-config --cflags --libs sqlite3
+  ls ${CROSS_PREFIX}/lib | grep libsqlite3.a
 }
 
 prepare_c_ares() {
