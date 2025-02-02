8100116619d3dc917f1feab95e6f694ecfded533
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-02-02 10:47:29 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -41,11 +41,6 @@ echo "x86_64-w64-mingw32-gcc版本是："
 x86_64-w64-mingw32-gcc --version
 #x86_64-w64-mingw32-gcc -print-search-dirs
 
-echo "测试"
-echo 'int main() { return 0; }' > test.c
-x86_64-w64-mingw32-gcc test.c -o test.exe
-file test.exe  # 应输出 "PE32+ executable (console) x86-64"
-echo "测试结束"
 # 配置 apt 以保留下载的 .deb 包，并禁用 HTTPS 证书验证
 #rm -f /etc/apt/apt.conf.d/*
 #echo 'Binary::apt::APT::Keep-Downloaded-Packages "true";' >/etc/apt/apt.conf.d/01keep-debs
@@ -83,8 +78,8 @@ curl -L https://ftp.gnu.org/gnu/gmp/gmp-${gmp_tag}.tar.xz | tar x --xz
 cd gmp-*
 # patch configure
 sed -i \
--e 's/ac_prog=`(\$CC -print-prog-name=ld) 2>&5 | tr -d '\\\'015'` ;;/ac_prog=`("\$CC" -print-prog-name=ld) 2>&5 | sed \'s/\\\\r$//\'` ;;/' \
--e 's/ac_prog=`(\$CC -print-prog-name=ld) 2>&5` ;;/ac_prog=`("\$CC" -print-prog-name=ld) 2>&5 | sed \'s/\\\\r$//\'` ;;/' \
+-e 's/ac_prog=`(\\$CC -print-prog-name=ld) 2>&5 | tr -d \'\\\\\\\'015\'` ;;/ac_prog=`("\\$CC" -print-prog-name=ld) 2>&5 | sed \'s/\\\\\\\\r$//\'` ;;/g' \
+-e 's/ac_prog=`(\\$CC -print-prog-name=ld) 2>&5` ;;/ac_prog=`("\\$CC" -print-prog-name=ld) 2>&5 | sed \'s/\\\\\\\\r$//\'` ;;/g' \
 configure
 CC=x86_64-w64-mingw32-gcc ./configure \
     --disable-shared \
