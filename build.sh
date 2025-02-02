6cc79b7d4c84374b238b2cb8e2a0ccb202afd7a1
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-02-02 16:22:55 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -80,35 +80,38 @@ curl -o configure https://raw.githubusercontent.com/rzhy1/aria2-static-build/ref
 
 # patch configure（不检测long long）
 perl -0777 -pi -e '
-# 处理每个外层 if 块
-BEGIN { $in_if = 0; }
+# 标记是否在外层 if 块中
+BEGIN { $in_if = 0; $delete_block = 0; }
+
+# 如果找到了外层 if 的起始
 if (/^if test "\$gmp_prog_cc_works" = yes; then/) {
     $in_if = 1;
-    $delete_block = 0;  # 默认不删除
+    $delete_block = 0;  # 初始时不删除
 }
 
-# 当在 if 块中，检查每行
+# 如果在 if 块中，检查每一行
 if ($in_if) {
-    if (/long long reliability test [12], program does not run/) {
-        $delete_block = 1;
+    # 如果找到 long long reliability test，触发删除
+    if (/long long reliability test/) {
+        $delete_block = 1;  # 设置删除标记
     }
 }
 
-# 如果找到了触发条件，则删除该 if 块
-if ($in_if && $delete_block) {
-    # 寻找闭合的 fi
+# 如果标记为删除，则跳过该行
+if ($delete_block) {
     if (/^fi$/) {
-        $in_if = 0;
-        $delete_block = 0;  # 重置标记
+        $in_if = 0;  # 找到 fi，退出删除块
+        $delete_block = 0;  # 重置删除标记
     }
-    next;  # 删除该行
+    next;  # 删除当前行
 }
 
-# 结束当前 if 块的判断
+# 如果遇到 fi，退出 if 块
 if (/^fi$/ && $in_if) {
-    $in_if = 0;  # 退出 if 块
+    $in_if = 0;  # 退出外层 if 块
 }
 ' configure
+
 
 echo "检查"
 bash -n configure
