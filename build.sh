252528d32e762af37814874e628db37e70333041
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-02-02 10:10:03 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -76,7 +76,7 @@ gmp_tag="$(retry curl -s https://ftp.gnu.org/gnu/gmp/ | grep -oE 'href="gmp-[0-9
 echo "gmp最新版本是${gmp_tag} ，下载地址是https://ftp.gnu.org/gnu/gmp/gmp-${gmp_tag}.tar.xz"
 curl -L https://ftp.gnu.org/gnu/gmp/gmp-${gmp_tag}.tar.xz | tar x --xz
 cd gmp-*
-./configure \
+CC=x86_64-w64-mingw32-gcc ./configure \
     --disable-shared \
     --enable-static \
     --prefix=$PREFIX \
