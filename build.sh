252d81ec16dca5d1bfe139d4d0c01d6439fac3f8
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-02-03 20:41:24 +0800

    ln -s $(which lld-link) /usr/bin/x86_64-w64-mingw32-ld.lld

--- build.sh
+++ build.sh
@@ -37,13 +37,14 @@ else
 fi
 end_time=$(date +%s.%N)
 duration1=$(echo "$end_time - $start_time" | bc | xargs printf "%.1f")
-ln -s $(which ld.lld) /usr/bin/x86_64-w64-mingw32-ld.lld
+#ln -s $(which ld.lld) /usr/bin/x86_64-w64-mingw32-ld.lld
+ln -s $(which lld-link) /usr/bin/x86_64-w64-mingw32-ld.lld
 
 echo "x86_64-w64-mingw32-gcc版本是："
 x86_64-w64-mingw32-gcc --version
 #x86_64-w64-mingw32-gcc -print-search-dirs
 echo "查询"
-which lld-link
+#which lld-link
 echo "查询结束"
 
 # 配置 apt 以保留下载的 .deb 包，并禁用 HTTPS 证书验证
