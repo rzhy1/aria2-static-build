3444f9902de2fddfec4698a371b47a32c6c7f265
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-02-03 16:53:54 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -49,9 +49,8 @@ else
 fi
 echo "x86_64-w64-mingw32-gcc版本是："
 x86_64-w64-mingw32-gcc --version
-which x86_64-w64-mingw32-ld.lld
 
-BUILD_ARCH="$(gcc -dumpmachine)"
+BUILD_ARCH="$(x86_64-w64-mingw32-gcc -dumpmachine)"
 TARGET_ARCH="${CROSS_HOST%%-*}"
 TARGET_HOST="${CROSS_HOST#*-}"
 case "${TARGET_ARCH}" in
