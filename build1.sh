80a0672b79c6011195f4cde62b4d64e59eff5464
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-02-03 18:14:44 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -50,12 +50,9 @@ fi
 echo "x86_64-w64-mingw32-gcc版本是："
 x86_64-w64-mingw32-gcc --version
 echo "查询"
-ls -l /usr/bin | grep x86_64-w64-mingw32
-echo "查询1"
-ls -l ${CROSS_ROOT} | grep x86_64-w64-mingw32
 ls -l /usr/bin | grep gold
 echo "查询2"
-ls -l ${CROSS_ROOT}/bin | grep x86_64-w64-mingw32
+#ls -l ${CROSS_ROOT}/bin | grep x86_64-w64-mingw32
 echo "查询结束"
 
 BUILD_ARCH="$(x86_64-w64-mingw32-gcc -dumpmachine)"
