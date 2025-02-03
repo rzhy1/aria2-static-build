aaa7ada053f751cb0c500aae7a6ea0e829a5cb92
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-02-03 14:04:54 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -58,7 +58,8 @@ which x86_64-w64-mingw32-gcc
 find / -name "x86_64-w64-mingw32-gcc"
 which lld
 which gold
-find / -name "*mingw*lld" 2>/dev/null
+find / -type f -name "*lld*" 2>/dev/null
+find / -type f -name "*gold*" 2>/dev/null
 which x86_64-w64-mingw32-gold
 which x86_64-w64-mingw32-lld
 echo "查询结束"
