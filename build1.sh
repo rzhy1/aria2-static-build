6a4b4772815bfee153b613777e5eaa843ecacece
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-02-04 20:07:29 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -247,10 +247,10 @@ prepare_sqlite() {
     ln -sf mksourceid.exe mksourceid
     SQLITE_EXT_CONF="config_TARGET_EXEEXT=.exe"
   fi
-  export LDFLAGS="$LDFLAGS -L/usr/x86_64-w64-mingw32/lib -lpthread"
-  export LIBS="$LIBS -lpthread"
+  export LDFLAGS="$LDFLAGS -L/usr/x86_64-w64-mingw32/lib -lwinpthread"
+  export LIBS="$LIBS -lwinpthread"
   echo "查询1"
-  ld -L/usr/x86_64-w64-mingw32/lib -lwinpthread --verbose
+  #ld -L/usr/x86_64-w64-mingw32/lib -lwinpthread --verbose
   echo "查询结束1"
   ./configure --build="${BUILD_ARCH}" --host="${CROSS_HOST}" --prefix="${CROSS_PREFIX}" --disable-shared  "${SQLITE_EXT_CONF}" \
     --enable-threadsafe \
