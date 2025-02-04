e7a6acfc0b9ce8954b933e40d6d88b20bdc8e470
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-02-04 19:05:26 +0800

    find / -name "*pthread.pc"

--- build1.sh
+++ build1.sh
@@ -55,6 +55,7 @@ x86_64-w64-mingw32-gcc --version
 echo "查询"
 find / -name "*pthread.a"
 find / -name "*pthread.h"
+find / -name "*pthread.pc"
 echo "查询结束"
 BUILD_ARCH="$(x86_64-w64-mingw32-gcc -dumpmachine)"
 TARGET_ARCH="${CROSS_HOST%%-*}"
@@ -252,7 +253,6 @@ prepare_sqlite() {
   export LDFLAGS="$LDFLAGS -lwinpthread"
   export LIBS="$LIBS -lwinpthread"
   echo "查询"
-  which x86_64-w64-mingw32-pkg-config
   x86_64-w64-mingw32-pkg-config --libs winpthreads
   x86_64-w64-mingw32-gcc -print-file-name=libwinpthread.a
   echo "查询结束"
