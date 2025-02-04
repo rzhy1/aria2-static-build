da695fe7956f3d9dcba7fd89f7a3df2db1ee3f0b
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-02-05 01:38:41 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -35,8 +35,8 @@ sudo dpkg --add-architecture i386
 export DEBIAN_FRONTEND=noninteractive
 # keep debs in container for store cache in docker volume
 sudo rm -f /etc/apt/apt.conf.d/*
-echo 'Binary::apt::APT::Keep-Downloaded-Packages "true";' >/etc/apt/apt.conf.d/01keep-debs
-echo -e 'Acquire::https::Verify-Peer "false";\nAcquire::https::Verify-Host "false";' >/etc/apt/apt.conf.d/99-trust-https
+sudo echo 'Binary::apt::APT::Keep-Downloaded-Packages "true";' >/etc/apt/apt.conf.d/01keep-debs
+sudo echo -e 'Acquire::https::Verify-Peer "false";\nAcquire::https::Verify-Host "false";' >/etc/apt/apt.conf.d/99-trust-https
 
 echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - 下载最新版mingw-w64⭐⭐⭐⭐⭐⭐"
 USE_GCC15=0
