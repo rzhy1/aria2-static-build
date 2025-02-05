8c57633bec26bf19bac4c8c922f24a1e6610a281
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-02-05 20:29:31 +0800

    Update build1.sh export LDFLAGS="$LDFLAGS -L/usr/x86_64-w64-mingw32/lib -lwinpthread"

--- build1.sh
+++ build1.sh
@@ -256,12 +256,10 @@ prepare_sqlite() {
   cp /usr/x86_64-w64-mingw32/lib/libwinpthread.a ${CROSS_PREFIX}/lib/
   #cp /usr/x86_64-w64-mingw32/include/pthread.h ${CROSS_PREFIX}/include/
   echo "显示内容"
-  #file ${CROSS_PREFIX}/lib/libwinpthread.a
-  #objdump -t ${CROSS_PREFIX}/lib/libwinpthread.a | grep pthread_create
+  file ${CROSS_PREFIX}/lib/libwinpthread.a
+  objdump -t ${CROSS_PREFIX}/lib/libwinpthread.a | grep pthread_create
   echo "显示内容"
-  #export CFLAGS="-I${CROSS_PREFIX}/include -mthreads"
-  #export CXXFLAGS="-I${CROSS_PREFIX}/include -mthreads"
-  export LDFLAGS="$LDFLAGS ${CROSS_PREFIX}/lib -lwinpthread"
+  export LDFLAGS="$LDFLAGS -L/usr/x86_64-w64-mingw32/lib -lwinpthread"
   export LIBS="-lwinpthread"
   #export ac_cv_search_pthread_create="-lwinpthread"
   #export ac_cv_search_pthread_mutexattr_init="-lwinpthread"
