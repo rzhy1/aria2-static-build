ff2f853c6a2bc85ef4116fa2001a80eb30f2767a
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-02-05 14:10:35 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -263,8 +263,8 @@ prepare_sqlite() {
   #export CXXFLAGS="-I${CROSS_PREFIX}/include -mthreads"
   #export LDFLAGS="$LDFLAGS -L/usr/x86_64-w64-mingw32/lib -lwinpthread"
   #export LIBS="-lwinpthread"
-  export ac_cv_search_pthread_create="-lwinpthread"
-  export ac_cv_search_pthread_mutexattr_init="-lwinpthread"
+  #export ac_cv_search_pthread_create="-lwinpthread"
+  #export ac_cv_search_pthread_mutexattr_init="-lwinpthread"
   ./configure \
     --build="${BUILD_ARCH}" --host="${CROSS_HOST}" --prefix="${CROSS_PREFIX}"  --disable-shared "${SQLITE_EXT_CONF}" \
     --enable-threadsafe \
@@ -274,9 +274,9 @@ prepare_sqlite() {
     --disable-rtree \
     --disable-session \
     --disable-editline \
-    --disable-load-extension
-    #ac_cv_search_pthread_create="-lpthread" \
-    #ac_cv_search_pthread_mutexattr_init="-lwinpthread"
+    --disable-load-extension \
+    ac_cv_search_pthread_create="-lpthread" \
+    ac_cv_search_pthread_mutexattr_init="-lwinpthread"
   make -j$(nproc)
   x86_64-w64-mingw32-ar cr libsqlite3.a sqlite3.o
   cp libsqlite3.a "${CROSS_PREFIX}/lib/" ||  exit 1
