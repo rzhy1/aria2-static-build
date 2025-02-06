dfe769de7285dd6cb9e95738149954242c402349
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-02-06 08:59:14 +0800

    Update build.shLIBS="-lwinpthread" \

--- build.sh
+++ build.sh
@@ -145,9 +145,10 @@ curl -L ${sqlite_latest_url} | tar xz
 #curl -L https://www.sqlite.org/2024/sqlite-autoconf-3470200.tar.gz | tar xz
 cd sqlite-*
 sudo ln -sf /usr/x86_64-w64-mingw32/lib/libwinpthread.a $PREFIX/lib/libwinpthread.a
-#LIBS="-lwinpthread" \
+
 #ac_cv_search_pthread_create="-lwinpthread" \
 #ac_cv_search_pthread_mutexattr_init="-lwinpthread"  \
+LIBS="-lwinpthread" \
 LDFLAGS="-L$PREFIX/lib -lwinpthread -flto" \
 ./configure \
     --disable-shared \
