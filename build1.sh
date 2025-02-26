2158dabb72e64e1258718e8a975131f03a40551e
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-02-26 22:48:43 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -253,9 +253,7 @@ prepare_sqlite() {
     SQLITE_EXT_CONF="config_TARGET_EXEEXT=.exe"
   fi
   local LDFLAGS="$LDFLAGS -L/usr/x86_64-w64-mingw32/lib -lwinpthread"
-  echo "查询1"
-  echo "查询结束1"
-  CFLAGS="$CFLAGS -DHAVE_PTHREAD" ./configure --prefix="${CROSS_PREFIX}" --disable-shared  "${SQLITE_EXT_CONF}" \
+  CFLAGS="$CFLAGS -DHAVE_PTHREAD" ./configure  --disable-shared  "${SQLITE_EXT_CONF}" \
     --enable-threadsafe \
     --disable-debug \
     --disable-fts3 --disable-fts4 --disable-fts5 \
