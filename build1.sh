bf777b875f50a4399b9f827f8332c3a942ad5b2c
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-02-26 22:58:32 +0800

    ac_cv_lib_m_ceil=yes

--- build1.sh
+++ build1.sh
@@ -253,7 +253,7 @@ prepare_sqlite() {
     SQLITE_EXT_CONF="config_TARGET_EXEEXT=.exe"
   fi
   local LDFLAGS="$LDFLAGS -L/usr/x86_64-w64-mingw32/lib -lwinpthread"
-  CFLAGS="$CFLAGS -DHAVE_PTHREAD" ./configure  --disable-shared  "${SQLITE_EXT_CONF}" \
+  CFLAGS="$CFLAGS -DHAVE_PTHREAD" ac_cv_lib_m_ceil=yes ./configure  --disable-shared  "${SQLITE_EXT_CONF}" \
     --enable-threadsafe \
     --disable-debug \
     --disable-fts3 --disable-fts4 --disable-fts5 \
