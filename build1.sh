2e92aae60dd9fcf77631a7ab551885c80ddc0c42
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-07-22 07:46:32 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -54,9 +54,9 @@ ln -s $(which lld-link) /usr/bin/x86_64-w64-mingw32-ld.lld
 echo "x86_64-w64-mingw32-gcc版本是："
 x86_64-w64-mingw32-gcc --version
 echo "查询"
-#find / -name "*pthread.a"
-#find / -name "*pthread.h"
-#find / -name "*pthread*.pc"
+find / -name "*pthread.a"
+find / -name "*pthread.h"
+find / -name "*pthread*.pc"
 echo "查询结束"
 BUILD_ARCH="$(x86_64-w64-mingw32-gcc -dumpmachine)"
 TARGET_ARCH="${CROSS_HOST%%-*}"
