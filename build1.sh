b2388cf0122a585d664bbc81584b3db9137d4667
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-07-22 08:00:31 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -266,8 +266,11 @@ prepare_sqlite() {
     ln -sf mksourceid.exe mksourceid
     SQLITE_EXT_CONF="config_TARGET_EXEEXT=.exe"
   fi
-  local LDFLAGS="$LDFLAGS -L/usr/x86_64-w64-mingw32/lib -lpthread"
-  CFLAGS="$CFLAGS -DHAVE_PTHREAD" ./configure --build="${BUILD_ARCH}" --host="${CROSS_HOST}" --prefix="${CROSS_PREFIX}" --disable-shared  "${SQLITE_EXT_CONF}" \
+  #local LDFLAGS="$LDFLAGS -L/usr/x86_64-w64-mingw32/lib -lwinpthread"
+  local export LDFLAGS="$LDFLAGS -L/usr/x86_64-w64-mingw32/lib"
+  local export LIBS="$LIBS -lwinpthread"
+  local export CFLAGS="$CFLAGS -DHAVE_PTHREAD -I/usr/x86_64-w64-mingw32/include"
+  ./configure --build="${BUILD_ARCH}" --host="${CROSS_HOST}" --prefix="${CROSS_PREFIX}" --disable-shared  "${SQLITE_EXT_CONF}" \
     --enable-threadsafe \
     --disable-debug \
     --disable-fts3 --disable-fts4 --disable-fts5 \
